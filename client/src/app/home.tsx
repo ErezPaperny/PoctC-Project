8a11040f2de36a03574bbab3cc22fac58cfbaba3
Erez Paperny <erezpap@gmail.com>
2024-12-29 00:00:17 +0200

    Adding the full use of Auth0

--- client/src/app/home.tsx
+++ client/src/app/home.tsx
@@ -3,17 +3,73 @@ import { HomeHeader } from './homeHeader'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { useUser } from '@auth0/nextjs-auth0/client'
+import axios from 'axios'
 
 export function Home() {
   const [message, setMessage] = useState('Loading...')
+  const [messagePrivate, setMessagePrivate] = useState('Loading...')
+  const [messagePrivatePre, setMessagePrivatePre] = useState('Loading...')
+  const [accessToken, setAccessToken] = useState()
   const { Header, Footer, Content } = Layout
   const { user, error, isLoading } = useUser()
 
   useEffect(() => {
-    fetch('http://localhost:5000')
-      .then((data) => data.text())
-      .then((dataText) => setMessage(JSON.parse(dataText).message))
-  })
+    const options = {
+      method: 'POST',
+      url: 'https://dev-twmpec4n6uralfn2.us.auth0.com/oauth/token',
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: 'rvPQ8gcHJfCedL2R97Ay57oV6A9Ev1FZ',
+        client_secret:
+          'AcWqnxqRZJKYZ5WbOyTAy6H4c9qBCP_CjukHLDjAxY6Q7xRIR2jbWgj8hxvJ_AcP',
+        audience: 'http://localhost:5000',
+      }),
+    }
+
+    if (user) {
+      axios
+        .request(options)
+        .then((response) => setAccessToken(response.data.access_token))
+    }
+  }, [user])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000')
+      .then((dataText) => setMessage(dataText.data.message))
+  }, [])
+
+  useEffect(() => {
+    if (accessToken) {
+      const optionsGet = {
+        method: 'GET',
+        url: 'http://localhost:5000/private',
+        headers: { authorization: `Bearer ${accessToken}` },
+      }
+
+      axios
+        .request(optionsGet)
+        .then((dataText) => setMessagePrivate(dataText.data.message))
+    }
+  }, [accessToken])
+
+  useEffect(() => {
+    if (accessToken) {
+      const optionsGet = {
+        method: 'GET',
+        url: 'http://localhost:5000/private-scope',
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+          scope: 'read:messages',
+        },
+      }
+
+      axios
+        .request(optionsGet)
+        .then((dataText) => setMessagePrivatePre(dataText.data.message))
+    }
+  }, [accessToken])
 
   return (
     <Layout style={{ height: '100vh' }}>
@@ -22,13 +78,25 @@ export function Home() {
         <div style={{ height: '300px' }}>
           <Flex vertical gap={'space-between'}>
             <HomeHeader />
-            {message}
+            Public : {message}
+            <br />
+            Private : {messagePrivate}
+            <br />
+            Private With Premission : {messagePrivatePre}
             <Button>
               <Link href="./users">User List</Link>
             </Button>
-            <br/>
-            {isLoading ? <>Auth0 Loading</> : error ? <>Error - {error.message}</> : user ? <>Welcome {user.name}</> : <>Not logged in</>}
-            <br/>
+            <br />
+            {isLoading ? (
+              <>Auth0 Loading</>
+            ) : error ? (
+              <>Error - {error.message}</>
+            ) : user ? (
+              <>Welcome {user.name}</>
+            ) : (
+              <>Not logged in</>
+            )}
+            <br />
             {!isLoading && !user ? (
               <Button>
                 <Link href="./api/auth/login">Login</Link>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,55 @@
 import express, { Request, Response } from 'express'
+import { requiredScopes, auth } from 'express-oauth2-jwt-bearer'
 
 const app = express()
 const PORT = 5000
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Content-Type, Authorization, Scope'
   )
+
   next()
 })
 
 app.get('/', (req: Request, res: Response) => {
   setTimeout(() => {
-    res.send({ message: 'Hello, world! This is message from server.' })
-  }, 1000) 
+    res.send({ message: 'Hello, world! This is global message from server.' })
+  }, 1000)
 })
+
+const checkJwt = auth({
+  audience: 'http://localhost:5000',
+  issuerBaseURL: `https://dev-twmpec4n6uralfn2.us.auth0.com/`,
+})
+
+app.get('/private', checkJwt, (req: Request, res: Response) => {
+  res.send({ message: 'Hello, world! This is private message from server.' })
+})
+
+const checkScopes = requiredScopes('read:messages')
+
+app.get(
+  '/private-scope',
+  checkJwt,
+  checkScopes,
+  (req: Request, res: Response) => {
+    const scope = req.headers.scope || 'no scope'
+    if (scope === 'read:messages') {
+      res.send({
+        message:
+          'Hello, world! This is private with scope of read message from server.',
+      })
+    } else {
+      res.send({
+        message: 'Missing read:messages scope',
+      })
+    }
+  }
+)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
